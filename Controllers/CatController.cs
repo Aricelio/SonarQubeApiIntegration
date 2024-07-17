17f909457f2f6cd0d85f4e0a2c5e5f9c46a7b8a3
Aricelio Fernandes <ariceliodesouza@gmail.com>
2024-07-16 21:54:33 -0300

    feat: added the cat controller

---
+++ Controllers/CatController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SonarQubeAPIIntegration.Domain;
+
+namespace SonarQubeAPIIntegration.Controllers
+{
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+        [HttpGet("api/v1/cats", Name = "Get Cats")]
+        public IEnumerable<Cat> GetCats()
+        {
+            return new List<Cat>
+            {
+                new Cat { Name = "Whiskers", Age = 3, Color = CatColor.Black },
+                new Cat { Name = "Mittens", Age = 7, Color = CatColor.White },
+                new Cat { Name = "Fluffy", Age = 2, Color = CatColor.Brown },
+                new Cat { Name = "Socks", Age = 5, Color = CatColor.Gray },
+                new Cat { Name = "Tiger", Age = 1, Color = CatColor.Orange }
+            };
+        }
+
+        [HttpGet("api/v1/cats/{name}", Name = "Get Cat By Name")]
+        public Cat GetCatByName([FromRoute] string name)
+        {
+            switch (name)
+            {
+                case "Whiskers":
+                    return new Cat { Name = "Whiskers", Age = 3, Color = CatColor.Black };
+
+                case "Mittens":
+                    return new Cat { Name = "Mittens", Age = 7, Color = CatColor.White };
+
+                case "Fluffy":
+                    return new Cat { Name = "Fluffy", Age = 2, Color = CatColor.Brown };
+
+                case "Socks":
+                    return new Cat { Name = "Socks", Age = 5, Color = CatColor.Gray };
+
+                case "Tiger":
+                    return new Cat { Name = "Tiger", Age = 1, Color = CatColor.Orange };
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ Domain/Cat.cs
@@ -0,0 +1,25 @@
+﻿namespace SonarQubeAPIIntegration.Domain
+{
+    /// <summary>Contains informations about cats</summary>
+    public class Cat
+    {
+        /// <summary>Gets or sets the name of the cat</summary>
+        public string Name { get; set; }
+
+        /// <summary>Gets or sets the age of the cat</summary>
+        public int Age { get; set; }
+
+        /// <summary>Gets or sets the color of the cat</summary>
+        public CatColor Color { get; set; }
+    }
+
+    /// <summary>The list of cat colors</summary>
+    public enum CatColor
+    {
+        Black,
+        White,
+        Brown,
+        Gray,
+        Orange
+    }
+}
